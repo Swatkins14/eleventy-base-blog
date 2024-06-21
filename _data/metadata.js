451404dc7de8c941e6714ec70021efc2aedaa0c3
Swatkins14 <samuel.w92@hotmail.co.uk>
2024-06-21 13:06:16 +0100

    fixed build error

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,11 @@ module.exports = {
 	title: "Sam Watkins Blog",
 	url: "https://samwatkins-blog.com",
 	language: "en",
-	description: "My name is Sam Watkins and this is my blog."
-	author: {
-		name: "Sam Watkins",
-		email: "samuel.w92@hotmail.co.uk",
-		url: "https://samwatkins-blog.com/about-me/"
+	description: "My name is Sam Watkins and this is my blog.",
+	author: {  
+	  name: "Sam Watkins",
+	  email: "samuel.w92@hotmail.co.uk",
+	  url: "https://samwatkins-blog.com/about-me/"
 	}
-}
+  };
+  
