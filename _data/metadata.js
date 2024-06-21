536039387f14aab158a10ea3a91d4571723d41f2
Swatkins14 <samuel.w92@hotmail.co.uk>
2024-06-21 12:46:29 +0100

    npm install

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Sam Watkins Blog",
 	url: "https://samwatkins-blog.com",
 	language: "en",
-	description: "My name is Sam Watkins and this is my blog.
+	description: "My name is Sam Watkins and this is my blog."
 	author: {
 		name: "Sam Watkins",
 		email: "samuel.w92@hotmail.co.uk",
